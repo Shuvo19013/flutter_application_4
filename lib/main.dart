113e3cb7d391dec123f8ca59f6d81674f0c81f7f
Shuvo19013 <shuvo15-5428@diu.edu.bd>
2025-02-18 10:48:27 +0600

    Wrap with Container and MainAxis,CrossAxis

--- lib/main.dart
+++ lib/main.dart
@@ -13,43 +13,50 @@ class Lab5 extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text("Basic Flutter UI-02"),
-          backgroundColor: const Color.fromARGB(255, 3, 16, 27),
+          backgroundColor: const Color.fromARGB(255, 168, 172, 170),
           centerTitle: true,
         ),
-        body: Row(
-          children: [
-            Icon(
-              Icons.star,
-              size: 40,
-              color: const Color.fromARGB(255, 27, 22, 7),
 
-            ),
-            Icon(
-              Icons.star,
-              size: 40,
-              color: const Color.fromARGB(255, 4, 23, 8),
-
-            ),
-            Icon(
-              Icons.star,
-              size: 40,
-              color: const Color.fromARGB(255, 8, 10, 20),
-
-            ),
-            Icon(
-              Icons.star,
-              size: 40,
-              color: const Color.fromARGB(255, 14, 4, 8),
-
-            ),
-            Icon(
-              Icons.star,
-              size: 40,
-              color: const Color.fromARGB(29, 238, 233, 235),
-
-            ),
-            
-          ],
+        body: Container(
+          height: double.infinity,
+          child: Row(
+          
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.star,
+                size: 40,
+                color: const Color.fromARGB(255, 27, 22, 7),
+          
+              ),
+              Icon(
+                Icons.star,
+                size: 40,
+                color: const Color.fromARGB(255, 4, 23, 8),
+          
+              ),
+              Icon(
+                Icons.star,
+                size: 40,
+                color: const Color.fromARGB(255, 8, 10, 20),
+          
+              ),
+              Icon(
+                Icons.star,
+                size: 40,
+                color: const Color.fromARGB(255, 14, 4, 8),
+          
+              ),
+              Icon(
+                Icons.star,
+                size: 40,
+                color: const Color.fromARGB(29, 94, 91, 92),
+          
+              ),
+              
+            ],
+          ),
         ),
       ),
     );
