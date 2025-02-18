4bba48b3ffea6cbd2356aca418153c9a22827716
Shuvo19013 <shuvo15-5428@diu.edu.bd>
2025-02-18 19:34:13 +0600

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -16,26 +16,18 @@ class Lab5 extends StatelessWidget {
           backgroundColor: const Color.fromARGB(255, 168, 172, 170),
           centerTitle: true,
         ),
-
         body: SingleChildScrollView(
           scrollDirection: Axis.horizontal,
-          child: Container(
-            height:800,
+          child: SizedBox(
+            height: 800,
             child: Row(
-            
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: List.generate(
-                100,
-              (index)=>Icon(
-                Icons.alarm,
-                size: index.toDouble(),
-                color:const Color.fromARGB(255, 131, 64, 255)
-              )
-               ),
-          
-        
-              
+                  100,
+                  (index) => Icon(Icons.alarm,
+                      size: index.toDouble(),
+                      color: const Color.fromARGB(255, 131, 64, 255))),
             ),
           ),
         ),
